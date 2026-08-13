ff0cf8927d97c413d1c87e6798a713a058d81694
Max Hayashi Batista <rm563717@fiap.com.br>
2026-08-13 19:15:54 -0300

    feat: created first backup file for database & fix: backup script now usable for docker container with named volume

--- db/scripts/03_backup_db.sql
+++ db/scripts/03_backup_db.sql
@@ -1,9 +1,6 @@
--- INSERIR CAMINHO DO ARQUIVO NA LINHA COMENTADA, EXEMPLO:
--- 'C:\Users\Chiemi Hayashi\Desktop\programacao\java\taskmanager\db\backups'
-
 USE taskmanager;
 GO
 
 EXEC ('BACKUP DATABASE [taskmanager] TO DISK = ''' +
-	'C:\Users\Chiemi Hayashi\Desktop\programacao\java\taskmanager\db\backups' + -- INSIRA APENAS O CAMINHO DA PASTA AQUI!!
-	'\taskmanager_bkp.bak'' WITH FORMAT, COMPRESSION');
+	'/var/opt/mssql/backup' + -- caminho interno do bind mount do docker
+	'/taskmanager_bkp.bak'' WITH FORMAT, COMPRESSION');
