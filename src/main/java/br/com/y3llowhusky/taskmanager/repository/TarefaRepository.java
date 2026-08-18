dc9ebecdde2d1eace1bacf5c5e0ae285113bf5b7
Max <hayashibatista@gmail.com>
2026-08-18 18:01:19 -0300

    feat: initiated tarefa repository logic

--- src/main/java/br/com/y3llowhusky/taskmanager/repository/TarefaRepository.java
+++ src/main/java/br/com/y3llowhusky/taskmanager/repository/TarefaRepository.java
@@ -1,5 +1,40 @@
 package br.com.y3llowhusky.taskmanager.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.y3llowhusky.taskmanager.model.Tarefa;
+import br.com.y3llowhusky.taskmanager.util.ConnectionFactory;
+
 public class TarefaRepository {
 
+	public List<Tarefa> listarTodas() {
+		
+		List<Tarefa> tarefas = new ArrayList<>();
+		Connection con = ConnectionFactory.getConnection();
+		String sql = "SELECT * FROM tarefas";
+		
+		try (con) {
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				
+				Tarefa tarefa = new Tarefa();
+				
+				tarefa.setNome(rs.getString("nome"));
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return tarefas;
+	}
+	
 }
